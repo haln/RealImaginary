4351866494521ad218405cc43cf537ec6a2a4927
JustinWalk <justinwalk@live.com>
2015-03-01 17:43:01 -0800

    House and Agent Class
    I'm super cool

---
+++ App_Code/Agent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Agent
+{
+	public Agent(string name, string phone, string email)
+	{
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+	}
+    public String name { get; set; }
+    public String phone { get; set; }
+    public String email { get; set; }
+}
