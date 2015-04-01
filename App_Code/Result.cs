a9c1898c79ee36de73d05576ab7d3c554c962b85
Casper Co <laqueenster@hotmail.com>
2015-03-31 20:12:14 -0700

    Search Engine Revision
    Search bar address filter now working properly. Filter data now stored
    as a session object.

---
+++ App_Code/Result.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Results
+/// </summary>
+public class Result
+{
+    public Result(string searchString, string bmin, string bmax, string c, string bedr, string bathr, string sft, string proptype)
+	{
+        searchStr = searchString;
+        budgetMin = bmin;
+        budgetMax = bmax;
+        city = c;
+        bedrooms = bedr;
+        bathrooms = bathr;
+        sqft = sft;
+        ptype = proptype;
+	}
+
+    public string searchStr { get; set; }
+    public string budgetMin { get; set; }
+    public string budgetMax { get; set; }
+    public string city { get; set; }
+    public string bedrooms { get; set; }
+    public string bathrooms { get; set; }
+    public string sqft { get; set; }
+    public string ptype { get; set; }
+}
